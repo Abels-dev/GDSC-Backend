91cc02ad90499813a93ec4cdcf61fdacb1fb8061
abel <abelgetachew143@gmail.com>
2024-08-12 20:08:49 +0300

    made some change

--- day15/question_1.js
+++ day15/question_1.js
@@ -30,24 +30,49 @@ db.person.aggregate([
 
 // Q3
 db.person.aggregate([
-    {
+   {
       $group: {
-        _id: {
-          gender: "$gender",   
-          isActive: "$isActive"    
-        },
-        count: { $sum: 1 }      
-      }
-    }
-  ]);
+         _id: {
+            gender: "$gender",
+            isActive: "$isActive",
+         },
+         count: { $sum: 1 },
+      },
+   },
+   {
+      $project: {
+         _id: 0,
+         gender: "$_id.gender",
+         isActive: "$_id.isActive",
+         count: 1,
+      },
+   },
+   {
+      $sort: {
+         gender: 1,
+      },
+   },
+]);
 
-  // Q4
+// Q4
 
-  db.person.aggregate([
-    {
+db.person.aggregate([
+   {
       $group: {
-        _id: "$company.location.country", 
-        averageAge: { $avg: "$age" } 
-      }
-    }])
-  
+         _id: "$company.location.country",
+         averageAge: { $avg: "$age" },
+      },
+   },
+   {
+      $project: {
+         _id: 0,
+         averageAge: 1,
+         country: "$_id",
+      },
+   },
+   {
+      $sort: {
+         country: 1,
+      },
+   },
+]);
